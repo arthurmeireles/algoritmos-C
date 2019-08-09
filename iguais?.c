210c0c293f2d329909b91e147a5a98aa3fac9c84
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-08-09 15:01:56 -0300

    Lista 02 - Resposta da Questão 03
    3. Desenvolva um algoritmo que leia dois n´umeros e informe se eles s˜ao iguais ou n˜ao.

--- iguais?.c
+++ iguais?.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void) {
+  float a, b;
+  scanf("%f", &a);
+  scanf("%f", &b);
+
+  if(a==b){
+    printf("Os números são iguais");
+  }
+  else{
+    printf("Os números não são iguais");
+  }
+  return 0;
+}
