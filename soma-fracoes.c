75456f86386f9170d117bd5562266e659f49db9b
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-08-20 10:04:15 -0300

    Correção de Erro
    O professor encontrou um erro. O erro era que o algoritmo estava arredondando a divisão pelos números não serem floats.
    Erro corrigido

--- soma-fracoes.c
+++ soma-fracoes.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main(void) {
-  float soma;
-  int n=1;
-  for(int i=1; i<=50; i++){
-    soma = soma + (n/i);
+  float soma, divisao;
+  float n=1;
+  for(int i=1; i<=4; i++){
+    divisao = n/i;
+    printf("divisao = %.2f \n ", divisao);    
+    soma = soma + divisao;
+    printf("soma %.2f \n", soma);    
     n=n+2;
+    printf("Numero que vai ser dividido = %f \n", n);    
   }
-  printf("%.2f ", soma);    
+  printf("Total = %.2f ", soma);    
   return 0;
 }
