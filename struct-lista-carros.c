bd3a455e59b8ddd43541c536f7f14417e12112e4
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-10-29 18:32:06 -0300

    Lista Struct - Questão 02
    Resolvendo erros de string

--- struct-lista-carros.c
+++ struct-lista-carros.c
@@ -1,10 +1,8 @@
-//falta resolver o problema do nome
-
 #include <stdio.h>
 #include <string.h>
 
 struct Carro{
-  // char marca[80], cor[80];
+  char marca[80], cor[80];
   int anoFabricacao, anoModelo;
   float preco;
 };
@@ -12,12 +10,12 @@ struct Carro{
 int main(void) {
   struct Carro carros[20];
   int contadorCarros = 0;
+  int entradaMenu;
   while(1>0){
     printf("Digite [0] para finalizar \n");
     printf("Digite [1] para listar \n");
     printf("Digite [2] para adicionar um carro \n");
 
-    int entradaMenu;
     scanf("%d", &entradaMenu);
 
     if(entradaMenu==0){
@@ -25,29 +23,30 @@ int main(void) {
     }
     if(entradaMenu==1){
       for(int i = 0; i < contadorCarros; i++){
-        // printf("Marca: %s \n", carros[i].marca);
-        // printf("Cor: %s \n", carros[i].cor);
- 
- 
+        printf("Marca: %s \n", carros[i].marca);
+        printf("Cor: %s \n", carros[i].cor);
         printf("Ano de Fabricação: %d \n", carros[i].anoFabricacao);
-     
         printf("Modelo do ano: %d \n", carros[i].anoModelo);
-
-        printf("Preço do carro: \n", carros[i].preco);
+        printf("Preço do carro: %.2f \n", carros[i].preco);
       }
     }
     if(entradaMenu==2){
-      struct Carro carro;
-      // carro.marca = 'batata';
-      // carro.cor = 'batata';
-      printf("Digite o ano de fabricação do carro:");
-      scanf("%d", &carro.anoFabricacao);
 
-      printf("Digite o ano do modelo:");
-      scanf("%d", &carro.anoModelo);
+      printf("Digite a marca do seu carro:\n");
+      scanf("%s", &carros[contadorCarros].marca);
+            
+      printf("Digite a cor do seu carro:\n");
+      scanf("%s", &carros[contadorCarros].cor);
 
-      printf("Digite o preço deste carro:");
-      scanf("%f", &carro.preco;
+      printf("Digite o ano de fabricação do carro:\n");
+      scanf("%d", &carros[contadorCarros].anoFabricacao);
+
+      printf("Digite o ano do modelo:\n");
+      scanf("%d", &carros[contadorCarros].anoModelo);
+
+      printf("Digite o preço deste carro:\n");
+      scanf("%f", &carros[contadorCarros].preco);
+
       contadorCarros++;
     }
   }
