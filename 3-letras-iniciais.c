80216d8b9fc24de097b23bf8b8d988d289f04969
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-11-17 19:14:15 -0300

    Resolve erros

--- 3-letras-iniciais.c
+++ 3-letras-iniciais.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main(void) {
   char nome[80];
   gets(nome);
-
-  printf("%c", nome[0]);  
-  printf("%c", nome[1]);
-  printf("%c", nome[2]);
+  int i = 0, qtd = 0;
+  while(nome[i] != '\0'){ 
+    if((nome[i] >= 'a' && nome[i] <= 'z') || (nome[i] >= 'A' && nome[i] <= 'Z')) qtd++;
+    i++;
+  }
+  printf("Quantidade de letras: %d\n", qtd);
   return 0;
 }
